f93a464321e65a0225741c82fd272dea93ab1bde
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2019-07-27 12:23:41 +0100

    Pass first byte as an argument to the encoder

--- 4/seq-xor-encoder.py
+++ 4/seq-xor-encoder.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import sys
+from random import randint
 
 shellcode = bytes.fromhex(sys.argv[1].replace('\\x', ''))
 
-output = [0xaa]
+if len(sys.argv) == 3:
+    output = [int(sys.argv[2], 0)]
+else:
+    output = [randint(1, 255)]
+
 for b in shellcode:
     xored_byte = b ^ output[-1]
     output.append(xored_byte)
